af6549e437d6440b0cfd2be6f3e6f47604cbc76c
wapxmas <yashin.sergey@gmail.com>
2016-08-11 00:01:46 +0300

    updates

--- app/Main.hs
+++ app/Main.hs
@@ -74,20 +74,20 @@ module Main where
   tpl1 :: Int @@ String
   tpl1 = 1 @@ "hello"
 
-  tpl2 :: Snd :=: (String @@ String)
+  tpl2 :: Snd :=: String @@ String
   tpl2 = tsnd ("hello" @@ "world")
 
-  tpl3 :: Snd :=: (String @@ String)
+  tpl3 :: Snd :=: String @@ String
   tpl3 = tsnd ("hello" @@ "world")
 
   tpl4 :: Int @@ String
   tpl4 = 1 @@ "hello"
 
-  tpl5 :: Snd :=: (String @@ (String @@ String))
+  tpl5 :: Snd :=: String @@ (String @@ String)
   tpl5 = tsnd ("hello" @@ ("hello" @@ "world"))
 
-  tpl6 :: Snd :=: Fst :=: Snd :=: ((String @@ String) @@ ((String @@ String) @@ String))
-  tpl6 = tsnd . tfst . tsnd $ (("world1" @@ "hello2") @@ (("hello3" @@ "world5") @@ "world4"))
+  tpl6 :: Snd :=: Fst :=: Snd :=: (String @@ String) @@ ((String @@ String) @@ String)
+  tpl6 = tsnd . tfst . tsnd $ ("world1" @@ "hello2") @@ (("hello3" @@ "world5") @@ "world4")
 
   main :: IO ()
   main = do
